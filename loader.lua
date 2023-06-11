c6c7d590bd2e09c1418d5e8b76e04fbb3ce5e70a
cunning <76720185+cunning-sys@users.noreply.github.com>
2023-06-10 20:22:54 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,17 @@
-function import(file)
-    return loadstring(game:HttpGet('https://raw.githubusercontent.com/cunning-sys/voxle-hub/main/'..file..'.lua'))()
-end
+repeat wait() until game:IsLoaded()
 
-import(game.PlaceId)
+game:GetService("Players").LocalPlayer.Idled:Connect(function()
+    game:GetService("VirtualUser"):ClickButton2(Vector2.new())
+end)
+
+local Main_Https = "https://raw.githubusercontent.com/cunning-sys/voxle-hub/main/"
+
+local games = {
+    [12521539843] = "Lee-RPG.lua"
+}
+
+for i, v in pairs(games) do
+    if i == game.PlaceId or i == game.GameId then
+        loadstring(game:HttpGet(Main_Https .. v))()
+    end
+end
